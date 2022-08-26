4660ee37adda5859257c24a459049d5c83189f81
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-08-26 15:29:48 -0300

    Update cursos.js

--- Atual/js/cursos.js
+++ Atual/js/cursos.js
@@ -4,9 +4,11 @@ const PacoteOffice = {
     "Imagem":"images/Cursos/1.jpg"
 }
 
-const auto_hidro_industrial = {"Nome":"AUTOMAÇÃO HIDRÁULICA INDUSTRIAL",
-"Descrição":"O Curso de Aperfeiçoamento Profissional Automação Hidráulica Industrial tem por objetivo o desenvolvimento de conhecimentos e habilidades básicas referentes aos componentes hidráulicos utilizados em máquinas, equipamentos e dispositivos.",
-"Imagem":"images/Cursos/Automação_Industrial.jpg"}
+const auto_hidro_industrial = {
+    "Nome":"AUTOMAÇÃO HIDRÁULICA INDUSTRIAL",
+    "Descrição":"O Curso de Aperfeiçoamento Profissional Automação Hidráulica Industrial tem por objetivo o desenvolvimento de conhecimentos e habilidades básicas referentes aos componentes hidráulicos utilizados em máquinas, equipamentos e dispositivos.",
+    "Imagem":"images/Cursos/Automação_Industrial.jpg"
+}
 
 const auto_pneu_industrial = {
     "Nome":"AUTOMAÇÃO PNEUMÁTICA INDUSTRIAL",
@@ -14,9 +16,17 @@ const auto_pneu_industrial = {
     "Imagem":"images/Cursos/CAI_Assistente_Administrativo.jpg"
 }
 
-const cursos = [PacoteOffice, auto_hidro_industrial,auto_pneu_industrial,auto_hidro_industrial,auto_pneu_industrial,PacoteOffice,PacoteOffice, auto_hidro_industrial,auto_pneu_industrial,auto_hidro_industrial,auto_pneu_industrial,PacoteOffice]
+const robos_automacao = {
+    "Nome":"ROBÔS INDUSTRIAIS - PROGRAMAÇÃO",
+    "Descrição":"O Curso de Aperfeiçoamento Profissional Robôs Industriais - Programação, tem por objetivo o desenvolvimento de competências que permitam identificar robôs inseridos em processos automatizados e suas principais aplicações no atual contexto da indústria 4.0, do mercado de robótica e comércio, bem como realizar programação seguindo procedimentos e normas técnicas, de qualidade, e de meio ambiente, saúde e segurança no trabalho.",
+    "Imagem":"images/Curso-de-programação.jpg"
+}
+
+const cursos = [robos_automacao, auto_hidro_industrial,auto_pneu_industrial,auto_hidro_industrial,auto_pneu_industrial,robos_automacao,robos_automacao, auto_hidro_industrial,auto_pneu_industrial,PacoteOffice,PacoteOffice,PacoteOffice]
 
 let tab = document.getElementById("v-pills-2")
+
+let g = 0
 
 for(let y = 0; y < 2; y++){
     let rowFic = document.createElement("div")
@@ -29,7 +39,7 @@ for(let y = 0; y < 2; y++){
     tab.appendChild(rowFic)
     rowFic.appendChild(Fic)
 
-    for(let x = 0; x < 6; x++){ 
+    for(let x = g; x < 6*(y+1); x++){ 
 
         let weq = document.getElementById(`FIC${y}`)
 
@@ -69,8 +79,14 @@ for(let y = 0; y < 2; y++){
         div3.appendChild(p1)
         div3.appendChild(p2)
         div3.appendChild(link2)
+
+        g++
     }
 }
+
+let fc3 = document.getElementById(`fic0`)
+fc3.className=""
+
 
 let divBotoes = document.createElement("div")
 divBotoes.className = "block-27"
@@ -86,7 +102,7 @@ let num_botoes = cursos.length/9
 let pag = document.getElementById("botoes-pagina")
 let esquerda =document.createElement("li")
 esquerda.innerHTML = "<a>&lt;</a>"
-pag.appendChild(esquerda)
+/* pag.appendChild(esquerda) */
 let direita =document.createElement("li")
 direita.innerHTML = "<a>&gt;</a>"
 
@@ -102,15 +118,17 @@ function trocaConteudo(a){
     let ewr = document.getElementById(a)
     ewr.className = "active"
     let fc = document.getElementById(`fic${a}`)
+    fc.style="display:block;"
 
     fc.className = ""
-    for(x=`${Number.parseInt(a)}`; x != 0; x--){
+    for(let x=(a - 1); x > -1; x--){
         let ewr = document.getElementById(x)
         ewr.className = ""
         let fc = document.getElementById(`fic${x}`)
         fc.style="display:none;"
     }
-    for(x=`${Number.parseInt(a)}`; x < 100; x++){
+
+    for(let x=(a + 1); x < cursos.length/9; x++){
         let ewr = document.getElementById(x)
         ewr.className = ""
         let fc = document.getElementById(`fic${x}`)
@@ -118,6 +136,4 @@ function trocaConteudo(a){
     }
 }
 
-
-
-pag.appendChild(direita)
+/* pag.appendChild(direita) */
