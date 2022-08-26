dd18f9628b4242d5986470851e9d7f9e1427ed3a
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-08-26 14:19:23 -0300

    Update cursos.js

--- Atual/js/cursos.js
+++ Atual/js/cursos.js
@@ -4,13 +4,24 @@ const PacoteOffice = {
     "Imagem":"images/Cursos/1.jpg"
 }
 
-const cursos = [PacoteOffice,PacoteOffice,PacoteOffice,PacoteOffice,PacoteOffice,PacoteOffice,PacoteOffice,PacoteOffice,PacoteOffice,PacoteOffice]
+const auto_hidro_industrial = {"Nome":"AUTOMAÇÃO HIDRÁULICA INDUSTRIAL",
+"Descrição":"O Curso de Aperfeiçoamento Profissional Automação Hidráulica Industrial tem por objetivo o desenvolvimento de conhecimentos e habilidades básicas referentes aos componentes hidráulicos utilizados em máquinas, equipamentos e dispositivos.",
+"Imagem":"images/Cursos/Automação_Industrial.jpg"}
+
+const auto_pneu_industrial = {
+    "Nome":"AUTOMAÇÃO PNEUMÁTICA INDUSTRIAL",
+    "Descrição":"O Curso de Aperfeiçoamento Profissional Automação Pneumática Industrial tem por objetivo o desenvolvimento de conhecimentos e habilidades básicas referentes aos componentes Pneumáticos utilizados em máquinas, equipamentos e dispositivos.",
+    "Imagem":"images/Cursos/CAI_Assistente_Administrativo.jpg"
+}
+
+const cursos = [PacoteOffice, auto_hidro_industrial,auto_pneu_industrial,auto_hidro_industrial,auto_pneu_industrial,PacoteOffice,PacoteOffice, auto_hidro_industrial,auto_pneu_industrial,auto_hidro_industrial,auto_pneu_industrial,PacoteOffice]
 
 let tab = document.getElementById("v-pills-2")
 
-for(let y = 0; y < cursos.length/9; y++){
+for(let y = 0; y < 2; y++){
     let rowFic = document.createElement("div")
     rowFic.id = `fic${y}`
+    rowFic.className = "remove"
     let Fic = document.createElement("div")
     Fic.id = `FIC${y}`
     Fic.className = "row"
@@ -19,6 +30,8 @@ for(let y = 0; y < cursos.length/9; y++){
     rowFic.appendChild(Fic)
 
     for(let x = 0; x < 6; x++){ 
+
+        let weq = document.getElementById(`FIC${y}`)
 
         let div1 = document.createElement("div")
         div1.className = "col-md-4 text-center"
@@ -48,7 +61,7 @@ for(let y = 0; y < cursos.length/9; y++){
         p2.className = "price"
         p2.textContent = "Presencial"
 
-        Fic.appendChild(div1)
+        weq.appendChild(div1)
         div1.appendChild(div2)
         div2.appendChild(link1)
         div2.appendChild(div3)
@@ -77,7 +90,13 @@ pag.appendChild(esquerda)
 let direita =document.createElement("li")
 direita.innerHTML = "<a>&gt;</a>"
 
-
+for(let x = 0; x < num_botoes; x++){
+    let link23 = document.createElement("li")
+    link23.innerHTML = `<a>${x}</a>`
+    link23.setAttribute("onclick", `trocaConteudo(${x})`)
+    link23.id = `${x}`
+    pag.appendChild(link23)
+}
 
 function trocaConteudo(a){
     let ewr = document.getElementById(a)
@@ -85,26 +104,20 @@ function trocaConteudo(a){
     let fc = document.getElementById(`fic${a}`)
 
     fc.className = ""
-    for(x=`${Number.parseInt(a)-1}`; x != 0; x--){
+    for(x=`${Number.parseInt(a)}`; x != 0; x--){
         let ewr = document.getElementById(x)
         ewr.className = ""
         let fc = document.getElementById(`fic${x}`)
-        fc.className = "remove"
+        fc.style="display:none;"
     }
-    for(x=`${Number.parseInt(a)+1}`; x < 100; x++){
+    for(x=`${Number.parseInt(a)}`; x < 100; x++){
         let ewr = document.getElementById(x)
         ewr.className = ""
         let fc = document.getElementById(`fic${x}`)
-        fc.className = "remove"
+        fc.style="display:none;"
     }
 }
 
-for(let x = 1; x < num_botoes + 1; x++){
-    let link23 = document.createElement("li")
-    link23.innerHTML = `<a>${x}</a>`
-    link23.setAttribute("onclick", `trocaConteudo(${x})`)
-    link23.id = `${x}`
-    pag.appendChild(link23)
-}
+
 
 pag.appendChild(direita)
