4132d675c7442a5aab11afc1604c936b0bffca22
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-10-16 23:56:20 -0300

    routes and modularizacao

--- Atual/app.js
+++ Atual/app.js
@@ -13,8 +13,40 @@ app.get("/classificados", async(req, res) => {
     res.render("classificados");
 })
 
-app.get("/home", async(req, res) => {
-    res.render("home");
+app.get("/cursos", async(req, res) => {
+    res.render("cursos");
+})
+
+app.get("/FormularioPadrao", async(req, res) => {
+    res.render("FormularioPadrao");
+})
+
+app.get("/servicos", async(req, res) => {
+    res.render("servicos");
+})
+
+app.get("/contato", async(req, res) => {
+    res.render("contato");
+})
+
+app.get("/GestaoQualidade", async(req, res) => {
+    res.render("GestaoQualidade");
+})
+
+app.get("/areaPedagogica", async(req, res) => {
+    res.render("areaPedagogica");
+})
+
+app.get("/ti", async(req, res) => {
+    res.render("ti");
+})
+
+app.get("/DocumentosRegulares", async(req, res) => {
+    res.render("DocumentosRegulares");
+})
+
+app.get("/sobre", async(req, res) => {
+    res.render("about");
 })
 
 app.use(express.static(__dirname + '/public'));
